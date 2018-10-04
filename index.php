95597702f3c925108dc39305d4e21f1189f68710
David Rushton <david@papertank.co.uk>
2018-10-04 10:29:09 +0100

    Removed semicolons.

--- index.php
+++ index.php
@@ -9,13 +9,12 @@
     <?php
     $pythonoutput = fopen("output.txt", "r") or die("Unable to open file");
     while (!feof($pythonoutput)) {
-        ;
         $tweet = fgets($pythonoutput);
         $parts = explode(":", $tweet, 2);
         $userName = explode("@", $parts[0], 2);
         $parts[0] = "@".$userName[1];
         echo "<div class='container'><br><br><br><br><span class='box'><li class='comment-body'><h5 class='username'>".$parts[0]."</h5>"."<p>".$parts[1]."</p>"."</li></span></div>";
-    };
+    }
     fclose($pythonoutput);
     ?>
     </ul>
