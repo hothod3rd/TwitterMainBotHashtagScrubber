5f3f6bc75e0bf01bf18e210184ac123357beaad7
David Rushton <david@papertank.co.uk>
2018-10-04 10:27:10 +0100

    Updated html id and classes to lowercase - https://google.github.io/styleguide/htmlcssguide.html\#General_Formatting_Rules

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="app.css">
 </head>
 <body>
-    <ul id="Tweets">
+    <ul id="tweets">
     <?php
     $pythonoutput = fopen("output.txt", "r") or die("Unable to open file");
     while (!feof($pythonoutput)) {
@@ -14,7 +14,7 @@
         $parts = explode(":", $tweet, 2);
         $userName = explode("@", $parts[0], 2);
         $parts[0] = "@".$userName[1];
-        echo "<div class='container'><br><br><br><br><span class='box'><li class='commentBody'><h5 class='username'>".$parts[0]."</h5>"."<p>".$parts[1]."</p>"."</li></span></div>";
+        echo "<div class='container'><br><br><br><br><span class='box'><li class='comment-body'><h5 class='username'>".$parts[0]."</h5>"."<p>".$parts[1]."</p>"."</li></span></div>";
     };
     fclose($pythonoutput);
     ?>
